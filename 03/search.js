fc8800f1c785e4d09e803dcd69721bc88b60fc54
정종호 <luxmikiove@gmail.com>
2015-04-08 23:04:13 +0900

    03
    03week

---
+++ 03/search.js
@@ -0,0 +1,28 @@
+var btnSearch = $('search');
+var addData = $('addMore');
+var pageNum = 1;
+
+btnSearch.addEventListener('click', search);
+addData.addEventListener('click', addPage);
+
+function addPage(event){
+  pageNum=pageNum+1;
+  search(event);
+  };
+
+function search(event){
+  var keyword = $('keyword').value;
+  var listTemplate = $('listTemplate').innerHTML;
+  var DaumAPI = "https://apis.daum.net/search/board?apikey=fdada071f31587e4d580f801f1a99542&q="+keyword+"&output=json&pageno="+pageNum;
+
+console.log(keyword);
+
+getJSON(DaumAPI , function(DaumData){
+
+  console.log(DaumData.channel.item)
+
+  var html = tmpl(listTemplate, {list: DaumData.channel.item});
+
+  $('wrap').innerHTML += html;
+});
+};
